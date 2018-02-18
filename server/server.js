70f8847a5c86ed7a37dd354b79b68860739c9cbc
ulibarri <carlos@progressly.com>
2018-02-17 18:29:45 -0600

    add GET/todos route

--- server/server.js
+++ server/server.js
@@ -18,6 +18,13 @@ app.post('/todos',(req,res)=>{
         res.status(400).send({err});
     });
 });
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    },(e)=>{
+        res.status(400).send({err});
+    });
+});
 if(!module.parent){
     app.listen(3000,()=>{
         console.log('server listening on port 3000');
